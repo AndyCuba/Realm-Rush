b5b6f04e238eafcfb8c78bdbfce3e573e25ac128
Andy Daytona <zfesandydaytona@gmail.com>
2021-10-21 01:00:59 +0300

    [Realm-Rush] coroutine added

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -6,19 +6,24 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
 
-    [SerializeField] List<Block> path;
+    [SerializeField] List<Waypoint> path;
     // Start is called before the first frame update
     void Start()
     {
-        PrintAllWaypoints();
+        StartCoroutine(FollowPath());
     }
 
-    private void PrintAllWaypoints()
+    private IEnumerator FollowPath()
     {
-        foreach (Block waypoint in path)
-        {       
-            print(waypoint.name);
+        print("Starting patroll..");
+
+        foreach (Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(1);
         }
+
+        print("Patrol is finished");
     }
 
     // Update is called once per frame
