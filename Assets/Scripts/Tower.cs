f7d6228fdef12741a781aac9809a98b62d327bc5
Andy Daytona <zfesandydaytona@gmail.com>
2021-10-26 20:02:09 +0300

    [Realm-Rush] enemy killing implemented

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,8 @@ public class Tower : MonoBehaviour
 {
     [SerializeField] Transform objectToMove;
     [SerializeField] Transform targetEnemy;
+    [SerializeField] float attackRange = 20f;
+    [SerializeField] ParticleSystem projectileParticle;
     void Start()
     {
         
@@ -14,6 +17,38 @@ public class Tower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        objectToMove.LookAt(targetEnemy);
+        if (targetEnemy)
+        {
+            objectToMove.LookAt(targetEnemy);
+            CheckForEnemy();
+        }
+        else
+        {
+            FireAtEnemy(false);
+        }
+    }
+
+    private void CheckForEnemy()
+    {
+        Vector3 enemyPos = targetEnemy.transform.position;
+        Vector3 towerPos = gameObject.transform.position;
+
+        float distanceToEnemy = Vector3.Distance(enemyPos, towerPos);
+
+        if (distanceToEnemy <= attackRange)
+        {
+            FireAtEnemy(true);
+        }
+        else
+        {
+            FireAtEnemy(false);
+        }
+    }
+
+    private void FireAtEnemy(bool isActive)
+    {
+        var emissionModule = projectileParticle.emission;
+
+        emissionModule.enabled = isActive;
     }
 }
