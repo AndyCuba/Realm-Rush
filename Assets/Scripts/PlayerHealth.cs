d406dd9f796392990b7b00f1a109093e59402bfe
Andrey Kuhorenko <zfesandydaytona@gmail.com>
2021-11-06 18:10:08 +0300

    [Realm-Rush] player health added, world space increased

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int health = 10;
+    [SerializeField] int healthDeacrease = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        health -= healthDeacrease;
+    }
+}
